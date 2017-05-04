a78641c3772334e7b161cc46a7a30f7de3f6f4ca
Chelsea Noriega <chelseanoriega@Chelseas-MacBook-Air.local>
2017-05-04 15:02:29 -0400

    picture changes, opactiy animation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 $(document).foundation()
 
 $(document).ready(function() {
-    var topOfOthDiv = $("#express").offset().top;
-
+    var topOfOthDiv = $("#express-section").offset().top;
     $(window).scroll(function() {
-        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-            $("#tablet").fadeTo(1800, 0.0); //reached the desired point -- show div
-         }
+        if($(window).scrollTop() > topOfOthDiv) {
+          $("#tablet").animate({opacity: 1}, 1800);
+        }
     });
 });
