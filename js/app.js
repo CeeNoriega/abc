3cc48309010e3d3c995f9bb0c466622e115b1abd
Chelsea Noriega <chelseanoriega@Chelseas-MacBook-Air.local>
2017-07-07 14:54:03 -0400

    no contracts img animation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 $(document).foundation()
 
 $(document).ready(function() {
+    var topOfthePage = $("#sitetotal").offset().top;
     var topOftheDiv = $("#recurring").offset().top;
+    $(window).scroll(function() {
+      if($(window).width() < 640) {
+        if($(window).scrollTop() > topOfthePage) {
+          $("#small-no-contracts").animate({opacity: 1}, 1800);
+        }
+      }
+      else{
+        if($(window).scrollTop() > topOfthePage) {
+          $("#no-contracts").animate({opacity: 1}, 1800);
+        }
+      }
+    });
     $(window).scroll(function() {
         if($(window).scrollTop() > topOftheDiv) {
           $("#tablet").animate({opacity: 1}, 1800);
