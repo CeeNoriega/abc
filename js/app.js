bac65d70b4236e1f259876338c3048e8e3d6bcc2
Chelsea Noriega <chelseanoriega@Chelseas-MacBook-Air.local>
2017-07-11 13:27:02 -0400

    turn off ga

--- js/app.js
+++ js/app.js
@@ -27,33 +27,33 @@ $(document).ready(function() {
 $('#learn-more').click(function(e) {
   var label = $(this).attr('id');
   var action = $(this).attr('data-ga-label');
-  ga('send', 'event', 'Click', action, label);
+  // ga('send', 'event', 'Click', action, label);
 });
 
 $('#set-up-today').click(function(e) {
   var label = $(this).attr('id');
   var action = $(this).attr('data-ga-label');
-  ga('send', 'event', 'Click', action, label);
+  // ga('send', 'event', 'Click', action, label);
 });
 
 $('#sign-up-today').click(function(e) {
   var label = $(this).attr('id');
   var action = $(this).attr('data-ga-label');
-  ga('send', 'event', 'Click', action, label);
+  // ga('send', 'event', 'Click', action, label);
 });
 
 
-function scrollAnalyticsHandler(direction, label) {
-  if (direction === 'down') {
-      ga('send', 'event', 'Section', 'Hit scrolling downwards', label, 1);
-  } else {
-    ga('send', 'event', 'Section', 'Hit scrolling upwards', label, 1);
-  }
-}
-
-var sectionWaypoints = $("section").not("#cover").waypoint({
-  handler: function(direction) {
-      var label = this.element.id;
-      scrollAnalyticsHandler(direction, label);
-  }
+// function scrollAnalyticsHandler(direction, label) {
+//   if (direction === 'down') {
+//       ga('send', 'event', 'Section', 'Hit scrolling downwards', label, 1);
+//   } else {
+//     ga('send', 'event', 'Section', 'Hit scrolling upwards', label, 1);
+//   }
+// }
+//
+// var sectionWaypoints = $("section").not("#cover").waypoint({
+//   handler: function(direction) {
+//       var label = this.element.id;
+//       scrollAnalyticsHandler(direction, label);
+//   }
 });
