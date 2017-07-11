9dadb5b6c6e92c0abb2e7786a57613104db16d54
Chelsea Noriega <chelseanoriega@Chelseas-MacBook-Air.local>
2017-07-11 11:20:44 -0400

    prepare for deployment

--- js/app.js
+++ js/app.js
@@ -1,23 +1,45 @@
 $(document).foundation()
 
+// Animation for Images //
 $(document).ready(function() {
-    var topOfthePage = $("#sitetotal").offset().top;
-    var topOftheDiv = $("#recurring").offset().top;
+    var siteTotalDiv = $("#sitetotal").offset().top;
+    var recurringDiv = $("#recurring").offset().top;
     $(window).scroll(function() {
       if($(window).width() < 640) {
-        if($(window).scrollTop() > topOfthePage) {
+        if($(window).scrollTop() > siteTotalDiv) {
           $("#small-no-contracts").animate({opacity: 1}, 1800);
         }
       }
-      else{
-        if($(window).scrollTop() > topOfthePage) {
+      else {
+        if($(window).scrollTop() > siteTotalDiv) {
           $("#no-contracts").animate({opacity: 1}, 1800);
         }
       }
     });
     $(window).scroll(function() {
-        if($(window).scrollTop() > topOftheDiv) {
+        if($(window).scrollTop() > recurringDiv) {
           $("#tablet").animate({opacity: 1}, 1800);
         }
     });
+});
+
+// Google Analytics //
+$('#learn-more').click(function(e) {
+    var label = $(this).closest('section').attr('id').toString();
+    ga('send', 'event', 'Click', 'Click', label); // Google Analytics
+});
+
+function scrollAnalyticsHandler(direction, label) {
+if (direction === 'down') {
+    ga('send', 'event', 'Section', 'Hit scrolling downwards', label, 1);
+} else {
+    ga('send', 'event', 'Section', 'Hit scrolling upwards', label, 1);
+}
+}
+
+var sectionWaypoints = $( "section" ).not("#cover").waypoint({
+  handler: function(direction) {
+      var label = this.element.id;
+      scrollAnalyticsHandler(direction, label);
+  }
 });
